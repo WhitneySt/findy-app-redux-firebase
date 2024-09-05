e0b322913546d1e3ad88e9517652d206a37aade1
WhitneySt <whitneystena418@gmail.com>
2024-09-05 12:05:28 -0500

    Implementación del Registro con email y contraseña

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -2,47 +2,61 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../../Firebase/firebaseConfig";
 
-export const createAccountThunk = createAsyncThunk("auth/createAccount", async ({ email, password, name, photo }) => {
-    const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+export const createAccountThunk = createAsyncThunk(
+  "auth/createAccount",
+  async ({ email, password, name, photo }) => {
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
     await updateProfile(auth.currentUser, {
-        displayName: name,
-        photoURL: photo
-    })
+      displayName: name,
+      photoURL: photo,
+    });
     return {
-        id: userCredentials.user.uid,
-        displayName: name,
-        email: email,
-        accessToken: userCredentials.user.accessToken,
-        photoURL: photo
-    }
-});
-
-
+      id: userCredentials.user.uid,
+      displayName: name,
+      email: email,
+      accessToken: userCredentials.user.accessToken,
+      photoURL: photo,
+    };
+  }
+);
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-        isAuthenticated: false,
-        user: null,
-        loading: false,
-        error: null
+  name: "auth",
+  initialState: {
+    isAuthenticated: false,
+    user: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    clearError: (state) => {
+      state.error = null;
     },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(createAccountThunk.pending, (state) => {
-            state.loading = true;
-            state.error = null
-        }).addCase(createAccountThunk.fulfilled, (state, action) => {
-            state.loading = false;
-            state.isAuthenticated = true;
-            state.user = action.payload;
-            state.error = null
-        }).addCase(createAccountThunk.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message
-        })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createAccountThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createAccountThunk.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAuthenticated = true;
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(createAccountThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
 
 const authReducer = authSlice.reducer;
 export default authReducer;
+
+export const { clearError } = authSlice.actions;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "../components/Layout/Layout";
+import Feed from "../pages/Feed/Feed";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import PostDetails from "../pages/PostDetails/PostDetails";
+import Profile from "../pages/Profile/Profile";
+
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Feed />} />
+          <Route path="post/:postId" element={<PostDetails />} />
+          <Route path="profile/:userId" element={<Profile />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Navigate to={'/' } /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
